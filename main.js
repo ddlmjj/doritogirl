114cf30c6a1fed3ad485e29dacf8e1d784978058
ddlmjj <vanderbekennoan@gmail.com>
2026-05-20 23:23:44 +0200

    Refonte dorito girl

---
+++ main.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const app = express();
+const port = 4000;
+
+app.use(express.static("assets"));
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/index.html");
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
